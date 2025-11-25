36edf9d5ff6812cf28cb9da31228e0e91ec3851a
Mahir <mkojak75@gmail.com>
2025-11-25 22:59:00 +0100

    Topbar

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,20 @@
-import { Box } from "@mui/material";
+"use client";
+
+import { Box, ThemeProvider, CssBaseline } from "@mui/material";
+import { useMode, ColorModeContext } from "@/theme";
+import Topbar from "@/views/global/Topbar";
 
 
 export default function Home() {
+  const [theme, colorMode] = useMode();
   return (
-    <Box >Hallo Dashboard
-      
-    </Box>
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <main className="content">
+          <Topbar/>
+        </main>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
   );
 }
